14ec6251f926efc8762b112c8e6a5881a1db1264
jeannot diambu <diambujeannot@gmail.com>
2023-04-14 13:29:07 +0100

    Nav bar

---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Signup from "../../components/Signup";
+import Navbar from "../../components/Navbar";
+
+function signup() {
+  return (
+    <>
+      <Navbar />
+      <Signup />
+    </>
+  );
+}
+
+export default signup;
