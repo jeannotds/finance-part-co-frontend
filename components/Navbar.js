65b5e91449943b68d05a81442b2de8b6d82494e8
jeannot diambu <diambujeannot@gmail.com>
2023-04-17 14:03:27 +0100

    Style menu__link navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ function Navbar() {
     <header className="header">
       <h2 className="logo">FinP</h2>
       <nav className="navbar">
-        <a className="menu-link" href="#">Home</a>
-        <a className="menu-link" href="#">About Us</a>
-        <a className="menu-link" href="#">Project</a>
+        <a className="menu__link" href="#">Home</a>
+        <a className="menu__link" href="#">About Us</a>
+        <a className="menu__link" href="#">Project</a>
         <a className="active" href="#">Sign Up</a>
       </nav>
     </header>
