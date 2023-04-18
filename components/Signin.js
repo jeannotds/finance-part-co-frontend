7ced2da64e3206500e10a2e3bab27df69adab57b
jeannot diambu <diambujeannot@gmail.com>
2023-04-18 10:48:31 +0100

    Improve : form sign uo

--- components/Signin.js
+++ components/Signin.js
@@ -18,36 +18,40 @@ function Signin() {
   return (
     <>
       <div className="sigin__container">
-        <div className="img-signup">
-          <h2>Form Sign In</h2>
-          <Image src={auth} alt="signup" className="auth__signin" />
-          <p className="message-signin">Connectes-toi pour avoir acces aux données.</p>
-        </div>
-        <form className="form__form">
-          
-          <div className="form__email">
-            <div>
-              <label for="email" className="col-form-label">
-                Email
-              </label>
-              <input type="text" className="form-control" id="email" />
+        <div className="blog__container">
+          <div className="img__signin">
+            <h2>Form Sign In</h2>
+            <Image src={auth} alt="signup" className="auth__signin" />
+            <p className="message__signin">Connectes-toi pour avoir acces aux données.</p>
+          </div>
+          <form className="form__form">
+            
+            <div className="form__email">
+              <div>
+                <label for="email" className="col-form-label">
+                  Email
+                </label>
+                <input type="text" className="form-control" id="email" />
+              </div>
             </div>
-          </div>
-          <div className="form__password">
-            <div>
-              <label for="password" className="col-form-label">
-                Password
-              </label>
-              <input type="text" className="form-control" id="password" />
+            <div className="form__password">
+              <div>
+                <label for="password" className="col-form-label">
+                  Password
+                </label>
+                <input type="text" className="form-control" id="password" />
+              </div>
             </div>
+            
+            <button type="submit" class="btn btn-primary form__button">Sign In</button>
+            <div className="from__google">
+                <a href="#" className="google"><FaGofore className="icon__google" />Sign in with Google</a>
+            </div>
+            <div className="link__signin">
+                <p className="msg__account">Do yo have account ? <a href="signup">sign in.</a></p>
+            </div>
+          </form>
           </div>
-          
-          <button type="submit" class="btn btn-primary form__button">Sign In</button>
-          <div className="from__google">
-              <a href="#" className="google"><FaGofore className="icon__google" />Sign in with Google</a>
-          </div>
-          <p className="msg__account">Do yo have account ? <a href="signup">sign in.</a></p>
-        </form>
       </div>
     </>
   );
