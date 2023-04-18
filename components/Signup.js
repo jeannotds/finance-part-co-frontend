fa82b19adea8cc9c1bc33cf4ff35547f27d0a0f2
jeannot diambu <diambujeannot@gmail.com>
2023-04-18 11:51:04 +0100

    Improve : signup form

--- components/Signup.js
+++ components/Signup.js
@@ -1,72 +1,88 @@
-
 import React from "react";
 import Image from "next/image";
 import auth from "../src/img/auth.jpeg";
 
-
 function Signup() {
   return (
     <>
-      <div className="containers">
-        <div className="img-signup">
-          <h2>Form Sign Up</h2>
-          <Image src={auth} alt="signup" className="auth" />
-          <p className="message-signup">
-          Creer un compte pour avoir acces aux informations
-          </p>
+      <div className="signup__container">
+        <div className="blog__signup-container">
+          <div className="img__signup">
+            <h2>Form Sign In</h2>
+            <Image src={auth} alt="signup" className="auth__signup" />
+            <p className="message__signin">
+              Connectes-toi pour avoir acces aux données.
+            </p>
+          </div>
+          <form className="form__form-signup">
+            <div className="form__name">
+              <div>
+                <label for="form__name" className="col-form-label">
+                  Email
+                </label>
+                <input type="text" className="form-control" id="form__name" />
+              </div>
+            </div>
+            <div className="form__firstname">
+              <div>
+                <label for="form__firstname" className="col-form-label">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="form__firstname"
+                />
+              </div>
+            </div>
+            <div className="form__email">
+              <div>
+                <label for="form__email" className="col-form-label">
+                  Email
+                </label>
+                <input type="text" className="form-control" id="form__email" />
+              </div>
+            </div>
+            <div className="form__phone">
+              <div>
+                <label for="form__phone" className="col-form-label">
+                  Phone Number
+                </label>
+                <input type="phone" className="form-control" id="form__phone" />
+              </div>
+            </div>
+            <div className="form__password">
+              <div>
+                <label for="form__password" className="col-form-label">
+                  Password
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="form__password"
+                />
+              </div>
+            </div>
+            <div className="form__cpassword">
+              <div>
+                <label for="form__cpassword" className="col-form-label">
+                  Confirm Password
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="form__cpassword"
+                />
+              </div>
+            </div>
+            <button type="submit" class="btn btn-primary form__button">
+              Submit
+            </button>
+            <div className="msg__account">
+              Do yo have account ? <a href="signin">Sign in.</a>
+            </div>
+          </form>
         </div>
-        <form className="form">
-          <div className="fullname">
-            <div>
-              <label for="firstname" className="col-form-label">
-                {" "}
-                Firstname{" "}
-              </label>
-              <input type="text" className="form-control form-firstname" id="firstname" />
-            </div>
-            <div>
-              <label for="name" className="col-form-label">
-                {" "}
-                Name
-              </label>
-              <input type="text" className="form-control form-name" id="name" />
-            </div>
-          </div>
-          <div className="email">
-            <div>
-              <label for="email" className="col-form-label">
-                Email
-              </label>
-              <input type="text" className="form-control" id="email" />
-            </div>
-          </div>
-          <div className="phone">
-            <div>
-              <label for="phone" className="col-form-label">
-                Phone Number
-              </label>
-              <input type="phone" className="form-control" id="phone" />
-            </div>
-          </div>
-          <div className="password">
-            <div>
-              <label for="password" className="col-form-label">
-                Password
-              </label>
-              <input type="text" className="form-control" id="password" />
-            </div>
-          </div>
-          <div className="password">
-            <div>
-              <label for="cpassword" className="col-form-label">
-                Confirm Password
-              </label>
-              <input type="text" className="form-control" id="cpassword" />
-            </div>
-          </div>
-          <button type="submit" class="btn btn-primary button">Submit</button>
-          <div className="msg__account">Do yo have account ? <a href="signin" >Sign in.</a></div>
-        </form>
       </div>
     </>
   );
